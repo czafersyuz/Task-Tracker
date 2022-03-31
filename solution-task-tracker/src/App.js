b1cbfc9233e6a056393ad249e23f49f4c5950357
czafersyuz <czafersyuz@gmail.com>
2022-03-31 13:38:51 +0300

    create component

--- solution-task-tracker/src/App.js
+++ solution-task-tracker/src/App.js
@@ -1,12 +1,15 @@
 
-// import "./App.css";
+import "./App.css";
+
+import Header from "./components/Header";
 
 
 function App() {
  
 
   return (
-    <div className="container">
+    <div className="App">
+      <Header title="TASK TRACKER"/>
       
     </div>
   );
---
+++ solution-task-tracker/src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Header = ({title}) => {
+  return (
+    <div>
+        <h1>{title}</h1>
+    </div>
+  )
+}
+// Header.defaultProps ={
+//     title:"task tarcker"
+// };
+
+//title ="task default" const headerin içine yazılır
+
+export default Header
